48d1d0b385e152507631b16b3c9eeab92d262dae
Pedro Silva <150725974+silvapedrodev@users.noreply.github.com>
2024-11-20 17:06:05 -0300

    function menu-mobile

--- js/mobile-navbar.js
+++ js/mobile-navbar.js
@@ -5,6 +5,9 @@ export function navBar() {
   const nav = document.querySelector(".nav-menu")
   const btnClose = document.querySelector(".btnClose")
   const btnOpen = document.querySelector(".btnOpen")
+  const btnMenu = document.querySelectorAll("nav li a")
+
+  // --- eventos do botão de menu mobile ---
 
   menu.addEventListener("click", () => {
     nav.classList.toggle("active")
@@ -12,4 +15,16 @@ export function navBar() {
     btnClose.classList.toggle("active")
     document.body.classList.toggle('no-scroll')
   })
+
+  // ---- Fecha o menu ao clicar em algum link --- 
+
+  btnMenu.forEach(button => {
+    button.addEventListener("click", () => {
+      document.body.classList.toggle('no-scroll')
+      nav.classList.toggle("active")
+      btnOpen.classList.toggle("active")
+      btnClose.classList.toggle("active")
+    })
+  })
+
 }
